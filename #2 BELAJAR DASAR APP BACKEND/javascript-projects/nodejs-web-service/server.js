07c7d71e37c30a8e09d23a078f8dbdd691b8e79f
abw <abw@laku.com>
2021-05-23 02:34:46 +0700

    Learn Method/Verb REquest

--- #2 BELAJAR DASAR APP BACKEND/javascript-projects/nodejs-web-service/server.js
+++ #2 BELAJAR DASAR APP BACKEND/javascript-projects/nodejs-web-service/server.js
@@ -4,7 +4,24 @@ const requestListener = (request, response) => {
     response.setHeader('Content-Type','text/html');
 
     response.statusCode = 200;
-    response.end('<h1>Hello HTTP Server</h1>');
+    // response.end('<h1>Hello HTTP Server</h1>');
+
+    const { method } = request;
+    if(method == 'POST'){
+        console.log('<h1>Hai POST</h1>');
+    }
+    
+    if(method == 'PUT'){
+        console.log('<h1>Hai PUT</h1>');
+    }
+
+    if(method == 'GET'){
+        console.log('<h1>Hai GET</h1>');
+    }
+
+    if(method == 'DELETE'){
+        console.log('<h1>Hai DELETE</h1>');
+    }
 };
 
 const server = http.createServer(requestListener);

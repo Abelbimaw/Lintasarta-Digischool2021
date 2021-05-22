d68eadc65bccace1ddcb6ce72598d4f203cb3dd0
abw <abw@laku.com>
2021-05-23 03:22:05 +0700

    Learn Routing Request

--- #2 BELAJAR DASAR APP BACKEND/javascript-projects/nodejs-web-service/server.js
+++ #2 BELAJAR DASAR APP BACKEND/javascript-projects/nodejs-web-service/server.js
@@ -2,27 +2,37 @@ const http = require('http');
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type','text/html');
-
     response.statusCode = 200;
     // response.end('<h1>Hello HTTP Server</h1>');
 
-    const { method } = request;
-    if(method == 'POST'){
-        let body = [];
+    const { url,method } = request;
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            const {name} = JSON.parse(body);
-            response.end(`<h1> Hai, ${name}!</h1>`);
-        });
-    }
+    if(url == '/'){
+        if(method == 'GET'){
+            response.end('<h1>Ini adalah HomePage</h1>');
+        } else {
+            response.end(`<h1> Halaman tidak dapat diakses dengan ${method} request</h1>`);
+        }
+    } else if (url == '/about'){
+        if(method == 'POST'){
+            let body = [];
     
-    if(method == 'GET'){
-        console.log('<h1>Hai GET</h1>');
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+    
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const {name} = JSON.parse(body);
+                response.end(`<h1> Hai, ${name}! ini adalah halaman about</h1>`);
+            });
+        } else if( method == 'GET'){
+            response.end('<h1> Halo! Ini adalah halaman about</h1>');
+        } else {
+            response.end(`<h1> Halaman ini tidak dapat diakses dengan ${method} request`);
+        }
+    } else {
+        response.end('<h1> Halaman tidak ditemukan</h1>');
     }
 };
 

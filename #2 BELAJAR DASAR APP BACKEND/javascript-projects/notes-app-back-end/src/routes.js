fc12765664d96ccda96929cee72f1ef2e7d28ee4
abw <abw@laku.com>
2021-05-23 09:14:14 +0700

    preview notes

--- #2 BELAJAR DASAR APP BACKEND/javascript-projects/notes-app-back-end/src/routes.js
+++ #2 BELAJAR DASAR APP BACKEND/javascript-projects/notes-app-back-end/src/routes.js
@@ -1,10 +1,22 @@
-const { addNoteHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNodeByHandler } = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
+  },
+
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNodeByHandler,
   },
 ];
 
